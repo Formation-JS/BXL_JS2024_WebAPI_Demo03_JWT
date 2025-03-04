92491d4b25c15d4ed2e18e5fb0b7cf5fd8c33960
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-04 14:08:38 +0100

    Login method 😺

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,44 @@
+// Fake data pour la démo
+const members = [
+    {
+        id: 1,
+        username: 'della',
+        password: 'Test1234=',
+        isAdmin: true
+    },
+    {
+        id: 2,
+        username: 'donald',
+        password: 'Test1234=',
+        isAdmin: false
+    }
+];
+
+
 const authController = {
 
     login: (req, res) => {
-        res.sendStatus(501);
+        const { username, password } = req.body;
+
+        //! Validation des données 
+        if (!username?.trim() || !password?.trim()) {
+            //TODO Utiliser des schemas de validations : Zod, Yup, Joi, ...
+            res.status(422).json({ error: 'Username and password is required !' });
+            return;
+        }
+
+        //! Vérification des credentials
+        const member = members.find(m => m.username === username);
+        if (!member || member.password !== password) {
+            res.status(400).json({ error: 'Invalid credentials !' });
+            return;
+        }
+
+        //TODO Générer le JWT
+        const token = 'JWT...';
+
+        //! Réponse à l'utilisateur
+        res.status(200).json({ token });
     },
 
     register: (req, res) => {
